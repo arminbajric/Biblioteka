e94fc99d63b7c8a1f753b99f1fffb3f90e3f6504
Armin Bajric <bajric.armin.3t1@gmail.com>
2018-08-08 17:25:40 +0200

    Biblioteka

--- src/biblioteka/Naslov.java
+++ src/biblioteka/Naslov.java
@@ -4,20 +4,12 @@ import java.sql.SQLException;
 
 public class Naslov extends BibliotekaBaza {
 	@Override
-	public void addNewRow(String upit)
+	public void executeSQL(String upit)
 	{
-	super.addNewRow(upit);	
+	super.executeSQL(upit);	
 	}
 	@Override
-	public void deleteRow(String upit)
-	{
-	super.addNewRow(upit);	
-	}
-	@Override
-	public void updateRow(String upit)
-	{
-	super.updateRow(upit);	
-	}
+	
 	public void pretraziNaslove(String naslov) throws SQLException
 	{
 		super.pretraziNaslove(naslov);
--- src/biblioteka/Pozajmljivanje.java
+++ src/biblioteka/Pozajmljivanje.java
@@ -4,11 +4,11 @@ import java.sql.SQLException;
 
 public class Pozajmljivanje extends BibliotekaBaza {
 	@Override
-	public void addNewRow(String upit) {
-		super.addNewRow(upit);
+	public void executeSQL(String upit) {
+		super.executeSQL(upit);
 	}
 
-	public boolean checkKnjiga(int ID,int korisnik) throws SQLException {
+	public boolean checkKnjiga(int ID, int korisnik) throws SQLException {
 		if (super.checkKnjiga(ID)) {
 			return true;
 
@@ -19,21 +19,15 @@ public class Pozajmljivanje extends BibliotekaBaza {
 	}
 
 	@Override
-	public void updateRow(String upit) {
-		super.updateRow(upit);
+
+	public void pozajmiKnjigu(String upit, int knjiga, int ID) {
+		super.pozajmiKnjigu(upit, knjiga, ID);
 	}
-	public void pozajmiKnjigu(String upit,int knjiga,int ID)
-	{
-		super.pozajmiKnjigu(upit,knjiga,ID);
-	}
-	public boolean checkPozajmljivanje(int ID) throws SQLException
-	{
-		if(super.checkPozajmljivanje(ID))
-		{
+
+	public boolean checkPozajmljivanje(int ID) throws SQLException {
+		if (super.checkPozajmljivanje(ID)) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
